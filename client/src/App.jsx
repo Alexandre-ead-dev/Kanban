7b29e9939bef39ce39a1702a41614bdd7a6c65a4
Alexandre <alexandre.gerard.dev@gmail.com>
2023-07-26 16:50:53 +0200

    darkmode hooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="text-4xl text-red-600">Test</div>;
+  return;
+  <div>
+    {/* HEADER */}
+    <Header />
+  </div>;
 }
 
 export default App;
