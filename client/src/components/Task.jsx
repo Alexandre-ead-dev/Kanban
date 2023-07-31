b9624de4c68c82964d42e51cec92a116ecde002d
Alexandre <alexandre.gerard.dev@gmail.com>
2023-07-31 18:50:01 +0200

    drag and drop api (drag and drop tasks)

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -18,12 +18,21 @@ function Task({ colIndex, taskIndex }) {
     }
   });
 
+  const handleOnDrag = (e) => {
+    e.dataTransfer.setData(
+      "text",
+      JSON.stringify({ taskIndex, prevColIndex: colIndex })
+    );
+  };
+
   return (
     <div>
       <div
         onClick={() => {
           setIsTaskModalOpen(true);
         }}
+        draggable
+        onDragStart={handleOnDrag}
         className=" w-[280px] first:my-5 rounded-lg  bg-white  dark:bg-[#2b2c37] shadow-[#364e7e1a] py-6 px-3 shadow-lg hover:text-[#38ada9] dark:text-white dark:hover:text-[#38ada9] cursor-pointer "
       >
         <p className=" font-bold tracking-wide ">{task.title}</p>
