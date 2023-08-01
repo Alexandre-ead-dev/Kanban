0a4cbe84f7f0667652813aa2595de09564d0c15d
Alexandre <alexandre.gerard.dev@gmail.com>
2023-08-01 18:28:44 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDatabase from "./MongoDb.js";
-import boardRouter from "./Routes/BoardRoute.js";
 
 // Création de l'application Express
 dotenv.config();
